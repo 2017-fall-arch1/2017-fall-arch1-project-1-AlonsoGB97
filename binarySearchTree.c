6f1659353592c339614c48e2612a5e8da147600f
student <student@localhost.localdomain>
2017-10-16 23:48:26 -0600

    Implement alloc in binarySearchTree.c

---
+++ binarySearchTree.c
@@ -0,0 +1,29 @@
+#include <studio.h>
+#include <stdlib.h>
+#include<assert.h>
+#include "binarySearchTree.h"
+
+
+
+/* create a new tree*/
+BST *llAlloc()
+{
+  BST *bs = (BST *)malloc(sizeof(BST));
+  bs -> head = 0;
+  return bs;
+}
+
+void insertItem(BST *bs, char *s)
+{
+
+}
+
+void removeItem(BST *bs)
+{
+
+}
+
+void printItems(BST *bs, char *msg)
+{
+
+}
